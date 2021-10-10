20cdf6d26c0cc2d59a334cc3e923065004b93388
po4an <43005263+po4an@users.noreply.github.com>
2021-10-10 20:13:11 +0300

    delete config.py and utils.py

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,27 @@
 from flask import Flask
 from flask import request
-from utils import add_action
+from os import getenv
+import psycopg2 as pg
 
+
+
+
+
+
+db_pass = getenv('DB_PASS')
+db_user = getenv('DB_USER')
+db_maindb = getenv('DB_MAINDB')
+db_host = getenv('DB_HOST')
+db_port = getenv('DB_PORT')
+
+def add_action(value):
+    conn = pg.connect(host=cf.db_host, dbname=cf.db_maindb, user=cf.db_user, password=cf.db_pass, port=cf.db_port)
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute(f"insert into public.log (value) values('{value}')")
+    finally:
+        conn.close()
 
 app = Flask(__name__)
 
@@ -14,4 +34,4 @@ def test():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=443)
--- flask/config.py
+++
@@ -1,7 +0,0 @@
-from os import getenv
-
-db_pass = getenv('DB_PASS')
-db_user = getenv('DB_USER')
-db_maindb = getenv('DB_MAINDB')
-db_host = getenv('DB_HOST')
-db_port = getenv('DB_PORT')
