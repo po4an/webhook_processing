4176652f3ab6d022b4da5cfe59b42bb2c2e3b849
po4an <43005263+po4an@users.noreply.github.com>
2021-11-21 13:20:35 +0300

    done

--- flask/app.py
+++ flask/app.py
@@ -1,39 +1,14 @@
-import psycopg2 as pg
-from flask import request
 from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
 
-# config
-db_host='postgres'
-db_user='postgres'
-db_pass='postgres'
-db_port='5432'
-db_maindb='postgres'
-port = 5432
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-def add_action(value):
-    conn = pg.connect(host=db_host, dbname=db_maindb, user=db_user, password=db_pass, port=db_port)
-    try:
-        with conn:
-            with conn.cursor() as cur:
-                cur.execute(f"insert into public.log (value) values('{value}')")
-    finally:
-        conn.close()
-
-
-
-
-@app.route('/', methods = ["POST", "GET"])
-def test():
-    if request.method == "POST":
-        value = str(request.json).replace("'", "`")
-        add_action(value)
-        return {"ok": True}
-    else:
-        return "Hello from grigoryBzr"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 443)
+    app.run(host='0.0.0.0', port=5050)
--- liquibase/migration.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --network=webhook_processing_default --rm -v C://Users//grish//PycharmProjects//testtest//webhook_processing//liquibase//:/liquibase/changelog liquibase/liquibase --driver=org.postgresql.Driver --url="jdbc:postgresql://postgres:5432/postgres" --username=postgres --password=postgres --changelogFile=root.changelog.xml update
